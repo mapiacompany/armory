38db0863e966c8312ea3daf175d537ae0b0920c0
Heo Sangmin <heo@mapiacompany.com>
2019-12-24 16:45:56 +0900

    feat(ngrx-async-status): add AsyncStatusType for type hinting

--- projects/armory/src/util/ngrx-async-status.ts
+++ projects/armory/src/util/ngrx-async-status.ts
@@ -1,7 +1,12 @@
+export interface AsyncStatusType {
+  pending: boolean;
+  fulfilled: boolean;
+  rejected: boolean;
+}
 
 export class AsyncStatus {
-  static INITIAL = { pending: false, fulfilled: false, rejected: false };
-  static PENDING = { pending: true, fulfilled: false, rejected: false };
-  static FULFILLED = { pending: false, fulfilled: true, rejected: false };
-  static REJECTED = { pending: false, fulfilled: false, rejected: true };
+  static INITIAL: AsyncStatusType = { pending: false, fulfilled: false, rejected: false };
+  static PENDING: AsyncStatusType = { pending: true, fulfilled: false, rejected: false };
+  static FULFILLED: AsyncStatusType = { pending: false, fulfilled: true, rejected: false };
+  static REJECTED: AsyncStatusType = { pending: false, fulfilled: false, rejected: true };
 }
