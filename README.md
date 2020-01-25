f2ec30d84ac8b29aa2085cb94d7c3094d51cc28e
Heo Sangmin <heo@mapiacompany.com>
2020-01-25 14:38:15 +0900

    docs: add mcash-payment code comment

--- README.md
+++ README.md
@@ -1,27 +1,28 @@
-# Armory
+# @mapiacompany/platform
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.0-next.9.
 
-## Development server
+| NPM Package  | Purpose | 
+|---| --- |
+| @mapiacompany/armory  | Angular + Typescript Utils |
+| @mapiacompany/ngx-translate-extend | @ngx-translate/core 라이브러리 기능을 확장해서 다국어 관리하기 편하게 만든 라이브러리 |
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+## @mapiacompany/armory
+[![npm version](https://badge.fury.io/js/%40mapiacompany%2Farmory.svg)](https://badge.fury.io/js/%40mapiacompany%2Farmory)
+### 지원하는 함수
+- NgRx createAction을 확장한 createActionExtend 함수 (BEGIN, SUCCESS, FAILURE)
+- 페이팔 결제 버튼 렌더링 + 결제 성공 제어
+- 모빌리언스 결제창을 띄워주는 함수 (팝업 차단에 대한 로직까지 추가)
 
-## Build
+## @mapiacompany/ngx-translate-extend
+[![npm version](https://badge.fury.io/js/%40mapiacompany%2Fngx-translate-extend.svg)](https://badge.fury.io/js/%40mapiacompany%2Fngx-translate-extend)
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+## @mapiacompany/components (Draft)
+- 마피아컴퍼니의 디자인 시스템 개발용 유틸 + 디자인 컴포넌트들을 정의합니다.
 
-## Running unit tests
+## @mapiacompany/dialog (Draft)
+- ngx-bootstrap modal을 대체할 모달을 구현하고 있습니다.
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+## @mapiacompany/carousel (Draft)
+- ngx-slick-carousel을 대체할 캐러셀을 구현하고 있습니다.
--- projects/armory/src/util/mcash-payment.ts
+++ projects/armory/src/util/mcash-payment.ts
@@ -1,3 +1,15 @@
+/*
+ * KG모빌리언스 Mcash 결제 창을 띄워주는 함수.
+ *
+ * 리팩토링한 대상 스크립트: https://mup.mobilians.co.kr/js/ext/ext_inc_comm.js
+ * 4년동안 한번도 바뀌지 않은 스크립트여서 아래처럼 타입스크립트 버전을 구현했습니다.
+ *
+ * 모빌리언스 문서에 의하면 https://pg.mcash.co.kr/dlp/js/npgIF.js 도 있지만 실제 결제 과정에서 사용하지 않으므로 로드하지 않음
+ *
+ * 작성자: Heo Sangmin
+ * 업데이트: 2020.01.25
+ */
+
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { finalize, tap } from 'rxjs/operators';
 import { enablePassive } from './event-passive';
@@ -16,7 +28,7 @@ const MOB_CASH_GB_DICT = {
   ra: 'RA'
 };
 
-function MCASH_MOBILE_FLAG() {
+function IS_MOBILE() {
   const UserAgent = navigator.userAgent;
   // tslint:disable-next-line:max-line-length
   return (UserAgent.match(/iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i) != null
@@ -25,16 +37,16 @@ function MCASH_MOBILE_FLAG() {
 
 export function OPEN_MCASH_PAYMENT(
   config: {
-    notiEmail: string,
+    notiEmail: string, // 결제 알림 받을 URL (결제자 이메일 말고 가맹점 이메일)
     urls: {
-      site?: string,
-      noti: string,
-      close: string,
-      ok: string,
-      fail: string
+      site?: string, // 도메인 주소를 입력해야함
+      noti: string, // 결제 성공 시 신호 보낼 API 주소
+      close: string, // 모빌리언스측의 결제 취소 버튼 눌렀을 때 이동할 URL (결제자가 보는 화면)
+      ok: string, // 결제 성공했을 때 이동할 URL (결제자가 보는 화면)
+      fail: string // 결제 실패 시 이동할 URL (결제자가 보는 화면)
     },
-    height: string | number,
-    environment?: 'production' | 'stage',
+    height: string | number, // 결제창 높이
+    environment?: 'production' | 'stage', // 설정하지 않았을 때 기본값은 production입니다.
     SVC_IDS: Partial<{
       CN: string,
       MC: string,
@@ -52,18 +64,16 @@ export function OPEN_MCASH_PAYMENT(
     uuid: string, // 주문 고유값
     name: string, // 상품명
     userId: string | number,
-    price: number,
+    price: number, // 가격 (KRW 기준)
     formTarget: 'iframe' | 'popup' | 'self',
     iframeName?: string // CALL_TYPE이 'iframe'일 때 유효
     MSTR?: string // 암호화 값
   }
 ): Observable<boolean> {
-  const mobile_flag = MCASH_MOBILE_FLAG();
-
   const form: HTMLFormElement = document.createElement('form');
   form.name = 'mcashMapiaForm';
   form.acceptCharset = 'UTF-8';
-  form.method = 'post';  // Post 방식
+  form.method = 'post';
   form.action =
     (config.environment !== 'stage')
       ? 'https://mup.mobilians.co.kr/MUP/goMcashMain.mcash'
@@ -92,6 +102,9 @@ export function OPEN_MCASH_PAYMENT(
     createInput(method + '_SVCID', config.SVC_IDS[method]);
   });
 
+  // MOB_CASH_GB_DICT에서 먼저 찾아보고 없으면 config에서 찾음. config.MOB_CASH_GB_DICT가 설정되어있지 않은 경우도 고려.
+  // CASH_GB가 TN일 때에 대한 처리는 되어있지 않으므로 추후에 구현 필요
+  // (https://mup.mobilians.co.kr/js/ext/ext_inc_comm.js에 있는데 안쓰는 코드인거같아서 구현하지 않음)
   const CASH_GB = MOB_CASH_GB_DICT[transaction.method] || (config.MOB_CASH_GB_DICT || {})[transaction.method];
   if (!CASH_GB) {
     throw new Error(`${transaction.method}에 대한 CASH_GB을 알 수 없습니다. \n첫번째 인자에 MOB_CASH_GB_DICT를 설정해주세요.`);
@@ -121,7 +134,7 @@ export function OPEN_MCASH_PAYMENT(
       form.target = 'PAY_WIN';
 
       let PAY_WIN;
-      if (mobile_flag) {
+      if (IS_MOBILE()) {
         PAY_WIN = window.open('', 'PAY_WIN', 'fullscreen=yes,toolbar=yes,menubar=yes,scrollbars=no,resizable=no');
         if (!PAY_WIN) {
           alert('브라우저에서 팝업 허용해주셔야 결제 진행이 가능합니다.');
@@ -139,6 +152,7 @@ export function OPEN_MCASH_PAYMENT(
       break;
   }
 
+  // 결제 시도 -> 취소 -> 결제 시도 하는 경우, 그전에 만들어둔 form 태그때문에 제대로 동작하지 않는 문제가 있어 지우기 작업 진행
   const prev: HTMLElement = document.querySelector(`form[name=${form.name}]`);
   if (prev) {
     prev.remove();
@@ -146,6 +160,23 @@ export function OPEN_MCASH_PAYMENT(
   document.body.appendChild(form);
   form.submit();
 
+  /*
+   * BehaviorSubject를 이용해서 첫 stream이 실행되도록 했습니다.
+   * isProcessing$가 iframeMssageReader를 등록하고, 삭제하는 과정을 모두 처리할 수 있도록 Rx 파이프를 활용한게 핵심입니다.
+   * 리팩토링하더라도 iframeMessageReader에 대한 처리 '책임'을 한 곳에서 제어할 수 있도록 해주면 좋을듯합니다.
+   *
+   * 결제 과정에서 모빌리언스측 화면에 있는 X (닫기) 버튼을 누르면 config.urls.cancel 로 이동하는데
+   * 이 URL로 이동했다는 것을 알아차리기 위해 iframeMessageReader라는 이벤트를 이용합니다.
+   * https://developer.mozilla.org/ko/docs/Web/API/Window/postMessage 원리
+   *
+   * config.urls.cancel에서 보여주는 response가 아래와 같이 postMessage를 호출하면
+   * ```javascript
+   * if (window.parent && window.parent !== window) {
+   *    window.parent.postMessage('CANCEL_MCASH_PAYMENT', '*');
+   * }
+   * ```
+   * iframeMessageReader가 메세지를 받아서 결제가 취소되었다는 stream을 전송하는 방식입니다.
+   */
   const isProcessing$ = new BehaviorSubject<boolean>(true);
   const iframeMessageReader = (e: MessageEvent) => {
     console.log('postMessage', e);
@@ -154,7 +185,6 @@ export function OPEN_MCASH_PAYMENT(
       isProcessing$.complete();
     }
   };
-
   return isProcessing$.asObservable().pipe(
     tap((isProcessing) => {
       if (isProcessing) {
