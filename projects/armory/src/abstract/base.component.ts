06eb22f8dadb281a54446629916ba69d24b39b62
Heo Sangmin <heo@mapiacompany.com>
2020-05-30 23:24:10 +0900

    fix(base): add Directive to AbstractBaseComponent

--- projects/armory/src/abstract/base.component.ts
+++ projects/armory/src/abstract/base.component.ts
@@ -1,6 +1,8 @@
-import { OnDestroy } from '@angular/core';
+import { Directive, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 
+@Directive()
+// tslint:disable-next-line:directive-class-suffix
 export abstract class AbstractBaseComponent implements OnDestroy {
   _sub: Subscription[] = [];
 
